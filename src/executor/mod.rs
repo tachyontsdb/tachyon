19c6013a059f1247a1ef209f8514acbbd5f0a51a
Rajat Patwari <rajatpatwari@gmail.com>
2024-06-05 17:45:07 -0400

    Finish most of the virtual machine

--- src/executor/mod.rs
+++ src/executor/mod.rs
@@ -1,5 +1,12 @@
-use crate::{common::Timestamp, storage::file::Cursor};
-use std::{path::PathBuf, sync::Arc};
+use crate::{
+    common::{Timestamp, Value},
+    storage::file::Cursor,
+};
+use std::{
+    collections::{HashMap, VecDeque},
+    path::PathBuf,
+    sync::Arc,
+};
 
 #[non_exhaustive]
 #[repr(u8)]
@@ -9,14 +16,14 @@ pub enum OperationCode {
     Init,
     Halt,
 
-    OpenRead, // { register_index: u64, stream_id: u64, from_timestamp: u64, to_timestamp: u64 } returns cursor,
+    OpenRead, // { cursor: u64, file_paths_array_idx: u64, start: u64, end: u64 } (returns cursor),
     CloseRead, // { cursor: u64 },
 
     Next, // { cursor: u64 },
 
     // GoPrevEntry, // { cursor: u64 },
     // GoGreaterTimestampEntry, // Seek { cursor: u64, timestamp: u64 }, (might not need)
-    FetchScalar, // { cursor: u64, to_register: u64 }
+    // FetchScalar, // { cursor: u64, to_register: u64 }
     FetchVector, // { cursor: u64, to_register_timestamp: u64, to_register_value: u64 },
 
     Goto,    // { address: u64 }
@@ -47,21 +54,204 @@ pub enum OperationCode {
 
 const NUM_REGS: usize = 10;
 
-#[repr(C, packed)]
+pub enum OutputValue {
+    Scalar(Value),
+    Vector((Timestamp, Value)),
+}
+
 pub struct Context {
-    pc: u64,
+    pc: usize,
     regs: [u64; NUM_REGS],
-    cursor: Cursor,
-    file_paths: Arc<[PathBuf]>,
+    file_paths_array: Arc<[Arc<[PathBuf]>]>,
+    cursors: HashMap<usize, Cursor>,
+    outputs: VecDeque<OutputValue>,
 }
 
 impl Context {
-    pub fn new(file_paths: Arc<[PathBuf]>, start: Timestamp, end: Timestamp) -> Self {
+    pub fn new(file_paths_array: Arc<[Arc<[PathBuf]>]>) -> Self {
         Self {
             pc: 0,
             regs: [0x00u64; NUM_REGS],
-            cursor: Cursor::new(file_paths.clone(), start, end).unwrap(),
-            file_paths,
+            file_paths_array,
+            cursors: HashMap::new(),
+            outputs: VecDeque::new(),
+        }
+    }
+
+    fn open_read(&mut self, cursor_idx: u64, file_paths_array_idx: u64, start: u64, end: u64) {
+        if self.cursors.contains_key(&(cursor_idx as usize)) {
+            panic!("Cursor key already used!");
+        }
+
+        let cursor = Cursor::new(
+            self.file_paths_array[file_paths_array_idx as usize].clone(),
+            start,
+            end,
+        )
+        .unwrap();
+        self.cursors.insert(cursor_idx as usize, cursor);
+    }
+
+    fn close_read(&mut self, cursor_idx: u64) {
+        self.cursors.remove(&(cursor_idx as usize));
+    }
+
+    fn next(&mut self, cursor_idx: u64) {
+        self.cursors.get_mut(&(cursor_idx as usize)).unwrap().next();
+    }
+
+    fn fetch_vector(&mut self, cursor_idx: u64) -> (Timestamp, Value) {
+        self.cursors[&(cursor_idx as usize)].fetch()
+    }
+}
+
+pub fn execute(mut context: Context, buffer: &[u8]) {
+    while context.pc < buffer.len() {
+        let opcode: OperationCode = unsafe { std::mem::transmute(buffer[context.pc]) };
+        context.pc += 1;
+
+        match opcode {
+            OperationCode::NoOperation => {}
+
+            OperationCode::Init => {}
+            OperationCode::Halt => {
+                return;
+            }
+
+            OperationCode::OpenRead => {
+                let cursor_idx =
+                    u64::from_le_bytes(buffer[context.pc..(context.pc + 8)].try_into().unwrap());
+                context.pc += 8;
+                let file_paths_array_idx =
+                    u64::from_le_bytes(buffer[context.pc..(context.pc + 8)].try_into().unwrap());
+                context.pc += 8;
+                let start = Timestamp::from_le_bytes(
+                    buffer[context.pc..(context.pc + 8)].try_into().unwrap(),
+                );
+                context.pc += 8;
+                let end = Timestamp::from_le_bytes(
+                    buffer[context.pc..(context.pc + 8)].try_into().unwrap(),
+                );
+                context.pc += 8;
+
+                context.open_read(cursor_idx, file_paths_array_idx, start, end);
+            }
+            OperationCode::CloseRead => {
+                let cursor_idx =
+                    u64::from_le_bytes(buffer[context.pc..(context.pc + 8)].try_into().unwrap());
+                context.pc += 8;
+
+                context.close_read(cursor_idx);
+            }
+
+            OperationCode::Next => {
+                let cursor_idx =
+                    u64::from_le_bytes(buffer[context.pc..(context.pc + 8)].try_into().unwrap());
+                context.pc += 8;
+
+                context.next(cursor_idx);
+            }
+            OperationCode::FetchVector => {
+                let cursor_idx =
+                    u64::from_le_bytes(buffer[context.pc..(context.pc + 8)].try_into().unwrap());
+                context.pc += 8;
+                let to_register_timestamp =
+                    u64::from_le_bytes(buffer[context.pc..(context.pc + 8)].try_into().unwrap());
+                context.pc += 8;
+                let to_register_value = Timestamp::from_le_bytes(
+                    buffer[context.pc..(context.pc + 8)].try_into().unwrap(),
+                );
+                context.pc += 8;
+
+                let (timestamp, value) = context.fetch_vector(cursor_idx);
+                context.regs[to_register_timestamp as usize] = timestamp;
+                context.regs[to_register_value as usize] = value;
+            }
+
+            OperationCode::Goto => {
+                let address =
+                    u64::from_le_bytes(buffer[context.pc..(context.pc + 8)].try_into().unwrap());
+                context.pc += 8;
+
+                context.pc = address as usize;
+            }
+            OperationCode::GotoEq => {
+                let address =
+                    u64::from_le_bytes(buffer[context.pc..(context.pc + 8)].try_into().unwrap());
+                context.pc += 8;
+                let register1 =
+                    u64::from_le_bytes(buffer[context.pc..(context.pc + 8)].try_into().unwrap());
+                context.pc += 8;
+                let register2 =
+                    u64::from_le_bytes(buffer[context.pc..(context.pc + 8)].try_into().unwrap());
+                context.pc += 8;
+
+                let value1 = context.regs[register1 as usize];
+                let value2 = context.regs[register2 as usize];
+                if value1 == value2 {
+                    context.pc = address as usize;
+                }
+            }
+            OperationCode::GotoNeq => {
+                let address =
+                    u64::from_le_bytes(buffer[context.pc..(context.pc + 8)].try_into().unwrap());
+                context.pc += 8;
+                let register1 =
+                    u64::from_le_bytes(buffer[context.pc..(context.pc + 8)].try_into().unwrap());
+                context.pc += 8;
+                let register2 =
+                    u64::from_le_bytes(buffer[context.pc..(context.pc + 8)].try_into().unwrap());
+                context.pc += 8;
+
+                let value1 = context.regs[register1 as usize];
+                let value2 = context.regs[register2 as usize];
+                if value1 != value2 {
+                    context.pc = address as usize;
+                }
+            }
+
+            OperationCode::OutputScalar => {
+                let from_register =
+                    u64::from_le_bytes(buffer[context.pc..(context.pc + 8)].try_into().unwrap());
+                context.pc += 8;
+
+                context
+                    .outputs
+                    .push_back(OutputValue::Scalar(context.regs[from_register as usize]));
+            }
+            OperationCode::OutputVector => {
+                let from_register_timestamp =
+                    u64::from_le_bytes(buffer[context.pc..(context.pc + 8)].try_into().unwrap());
+                context.pc += 8;
+                let from_register_value =
+                    u64::from_le_bytes(buffer[context.pc..(context.pc + 8)].try_into().unwrap());
+                context.pc += 8;
+
+                context.outputs.push_back(OutputValue::Vector((
+                    context.regs[from_register_timestamp as usize],
+                    context.regs[from_register_value as usize],
+                )));
+            }
+
+            OperationCode::LogicalNot => todo!(),
+            OperationCode::LogicalAnd => todo!(),
+            OperationCode::LogicalOr => todo!(),
+            OperationCode::LogicalXor => todo!(),
+            OperationCode::LogicalShiftLeft => todo!(),
+            OperationCode::LogicalShiftRight => todo!(),
+            OperationCode::ArithmeticAdd => todo!(),
+            OperationCode::ArithmeticAddImmediate => todo!(),
+            OperationCode::ArithmeticSubtract => todo!(),
+            OperationCode::ArithmeticSubtractImmediate => todo!(),
+            OperationCode::ArithmeticMultiply => todo!(),
+            OperationCode::ArithmeticMultiplyImmediate => todo!(),
+            OperationCode::ArithmeticDivide => todo!(),
+            OperationCode::ArithmeticDivideImmediate => todo!(),
+            OperationCode::ArithmeticRemainder => todo!(),
+            OperationCode::ArithmeticRemainderImmediate => todo!(),
         }
     }
 }
+
+#[cfg(test)]
+mod tests {}
