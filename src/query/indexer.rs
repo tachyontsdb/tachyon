ce3552bb40446e08d39e5bd3b4b73c3c003f2110
Srihari Vishnu <srihari.vishnu@gmail.com>
2024-06-27 13:54:24 -0400

    fix tests & clippy

--- src/query/indexer.rs
+++ src/query/indexer.rs
@@ -1,4 +1,9 @@
-use std::{collections::HashSet, fs::{self, ReadDir}, hash::Hash, result};
+use std::{
+    collections::HashSet,
+    fs::{self, ReadDir},
+    hash::Hash,
+    result,
+};
 
 use promql_parser::label::Matchers;
 use rusqlite::Connection;
@@ -7,29 +12,36 @@ use uuid::Uuid;
 
 use crate::common::Timestamp;
 
-
 #[derive(Serialize, Deserialize, Debug)]
 struct IdsEntry {
     ids: HashSet<u128>,
 }
 
 trait IndexerStore {
-    fn insert_new_id(&self, root_dir: &String, stream: &String, matchers: &Matchers);
-    fn get_ids(&self, conn: &Connection, name: &String, value: &String) -> IdsEntry;
-    fn get_stream_and_matcher_ids(&self, root_dir: &String, stream: &String, matchers: &Matchers) -> Vec<HashSet<u128>>;
+    fn insert_new_id(&self, root_dir: &str, stream: &str, matchers: &Matchers);
+    fn get_ids(&self, conn: &Connection, name: &str, value: &str) -> IdsEntry;
+    fn get_stream_and_matcher_ids(
+        &self,
+        root_dir: &str,
+        stream: &str,
+        matchers: &Matchers,
+    ) -> Vec<HashSet<u128>>;
 }
 
 struct SQLiteIndexerStore {}
 
 impl IndexerStore for SQLiteIndexerStore {
-    fn insert_new_id(&self, root_dir: &String, stream: &String, matchers: &Matchers) {
+    fn insert_new_id(&self, root_dir: &str, stream: &str, matchers: &Matchers) {
         let new_id = Uuid::new_v4();
 
-        let mut conn: Connection = Connection::open(format!("{}/indexer.sqlite", root_dir)).unwrap();
-        let mut stmt = conn.prepare("INSERT INTO mapping (name, value, ids) VALUES (?, ?, ?)").unwrap();
+        let mut conn: Connection =
+            Connection::open(format!("{}/indexer.sqlite", root_dir)).unwrap();
+        let mut stmt = conn
+            .prepare("INSERT INTO mapping (name, value, ids) VALUES (?, ?, ?)")
+            .unwrap();
 
         // update and insert for stream
-        let mut stream_ids: IdsEntry = self.get_ids(&conn, &"__name".to_owned(), &stream);
+        let mut stream_ids: IdsEntry = self.get_ids(&conn, "__name", stream);
         stream_ids.ids.insert(new_id.as_u128());
         let stream_id_str = serde_json::to_string(&stream_ids).unwrap();
         stmt.execute(["__name", stream, &stream_id_str]);
@@ -39,90 +51,107 @@ impl IndexerStore for SQLiteIndexerStore {
             let mut matcher_ids: IdsEntry = self.get_ids(&conn, &matcher.name, &matcher.value);
             matcher_ids.ids.insert(new_id.as_u128());
             let matcher_id_str = serde_json::to_string(&matcher_ids).unwrap();
-            stmt.execute([matcher.name.to_owned(), matcher.value.to_owned(), matcher_id_str]);
+            stmt.execute([
+                matcher.name.to_owned(),
+                matcher.value.to_owned(),
+                matcher_id_str,
+            ]);
         }
     }
 
-    fn get_ids(&self, conn: &Connection, name: &String, value: &String) -> IdsEntry {
-        let result = conn.query_row("SELECT ids FROM mapping WHERE name = ? AND value = ?", [name, value], |row| row.get::<usize, String>(0).map(|s| s));
+    fn get_ids(&self, conn: &Connection, name: &str, value: &str) -> IdsEntry {
+        let result = conn.query_row(
+            "SELECT ids FROM mapping WHERE name = ? AND value = ?",
+            [name, value],
+            |row| row.get::<usize, String>(0),
+        );
 
         match result {
-            Ok(stream_ids_str) => { 
-                serde_json::from_str(stream_ids_str.as_str()).unwrap()
+            Ok(stream_ids_str) => serde_json::from_str(stream_ids_str.as_str()).unwrap(),
+            Err(e) => IdsEntry {
+                ids: HashSet::new(),
             },
-            Err(e) => {
-                IdsEntry{ ids: HashSet::new() }
-            }
         }
     }
 
-    fn get_stream_and_matcher_ids(&self, root_dir: &String, stream: &String, matchers: &Matchers) -> Vec<HashSet<u128>> {
+    fn get_stream_and_matcher_ids(
+        &self,
+        root_dir: &str,
+        stream: &str,
+        matchers: &Matchers,
+    ) -> Vec<HashSet<u128>> {
         let mut ids: Vec<HashSet<u128>> = vec![];
 
         let conn: Connection = Connection::open(format!("{}/indexer.sqlite", root_dir)).unwrap();
-        
-        ids.push(self.get_ids(&conn, &"__name".to_owned(), &stream).ids);
+
+        ids.push(self.get_ids(&conn, "__name", stream).ids);
 
         for matcher in &matchers.matchers {
             ids.push(self.get_ids(&conn, &matcher.name, &matcher.value).ids);
         }
 
-        return ids;
+        ids
     }
 }
 
 struct Indexer {
     root_dir: String,
-    store: Box<dyn IndexerStore>
+    store: Box<dyn IndexerStore>,
 }
 
 impl Indexer {
     fn new(root_dir: String) -> Self {
         Self {
-            root_dir: root_dir,
-            store: Box::new(SQLiteIndexerStore{})
+            root_dir,
+            store: Box::new(SQLiteIndexerStore {}),
         }
     }
 
-    fn insert_new_id(&self, stream: &String, matchers: &Matchers) {
+    fn insert_new_id(&self, stream: &str, matchers: &Matchers) {
         self.store.insert_new_id(&self.root_dir, stream, matchers);
     }
 
-    fn get_intersecting_ids(&self, id_lists: &Vec<HashSet<u128>>) -> HashSet<u128> {
+    fn get_intersecting_ids(&self, id_lists: &[HashSet<u128>]) -> HashSet<u128> {
         let mut intersection: HashSet<u128> = HashSet::new();
 
         if !id_lists.is_empty() {
             for id in &id_lists[0] {
                 let mut is_in_others = true;
-    
+
                 for other in &id_lists[1..] {
-                    if other.contains(&id) {
+                    if !other.contains(id) {
                         is_in_others = false;
                         break;
                     }
                 }
-    
+
                 if is_in_others {
                     intersection.insert(*id);
                 }
             }
         }
 
-        return intersection;
+        intersection
     }
 
-    fn get_required_files(&self, stream: String, matchers: Matchers, start: Timestamp, end: Timestamp) -> Result<Vec<String>, &'static str> {
-        let id_lists = self.store.get_stream_and_matcher_ids(&self.root_dir, &stream, &matchers);
+    fn get_required_files(
+        &self,
+        stream: String,
+        matchers: Matchers,
+        start: Timestamp,
+        end: Timestamp,
+    ) -> Result<Vec<String>, &'static str> {
+        let id_lists = self
+            .store
+            .get_stream_and_matcher_ids(&self.root_dir, &stream, &matchers);
         let intersecting_ids = self.get_intersecting_ids(&id_lists);
 
         for id in intersecting_ids {
             println!("{}", id);
-
         }
         todo!()
-    }    
+    }
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -149,7 +178,7 @@ mod tests {
     #[test]
     fn test_get_stream_and_matcher_ids() {
         let mut conn = Connection::open("./tmp/indexer.sqlite").unwrap();
-        
+
         conn.execute(
             "
             CREATE TABLE if not exists mapping (
@@ -163,10 +192,15 @@ mod tests {
         .unwrap();
 
         let indexer = Indexer::new("tmp".to_string());
-        let matchers = Matchers::new(vec![Matcher::new(MatchOp::Equal, "app", "dummy"), Matcher::new(MatchOp::Equal, "service", "backend")]);
+        let matchers = Matchers::new(vec![
+            Matcher::new(MatchOp::Equal, "app", "dummy"),
+            Matcher::new(MatchOp::Equal, "service", "backend"),
+        ]);
 
-        indexer.insert_new_id(&"https".to_owned(), &matchers);
-        let ids = indexer.store.get_stream_and_matcher_ids(&"tmp".to_owned(), &"https".to_owned(), &matchers);
+        indexer.insert_new_id("https", &matchers);
+        let ids = indexer
+            .store
+            .get_stream_and_matcher_ids("tmp", "https", &matchers);
 
         println!("{:?}", ids);
     }
