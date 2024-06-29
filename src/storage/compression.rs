4cf50ba4e2a0b96c4b74de3fd892a3a32f73bc17
Srihari Vishnu <srihari.vishnu@gmail.com>
2024-06-29 11:35:29 -0400

    compression refactor

--- src/storage/compression.rs
+++ src/storage/compression.rs
@@ -1,20 +1,7 @@
-use std::{io::Read, mem::size_of};
-
-use crate::{
-    common::{Timestamp, Value},
-    utils::file_utils::FileReaderUtil,
+use std::{
+    io::{Read, Write},
+    mem::size_of,
 };
-
-use super::file::Header;
-
-const EXPONENTS: [usize; 4] = [1, 2, 4, 8];
-
-const VAR_U64_READERS: [fn(&[u8]) -> u64; 4] = [
-    FileReaderUtil::read_u64_1,
-    FileReaderUtil::read_u64_2,
-    FileReaderUtil::read_u64_4,
-    FileReaderUtil::read_u64_8,
-];
 
 use crate::{
     common::{Timestamp, Value},
@@ -62,38 +49,81 @@ impl CompressionUtils {
     Based on google compression algorithm: https://static.googleusercontent.com/media/research.google.com/en//people/jeff/WSDM09-keynote.pdf
 */
 
-    */
-    pub fn compress(mut values: &Vec<u64>) -> Vec<u8> {
-        let mut result: Vec<u8> = Vec::new();
+pub struct CompressionEngine<T: Write> {
+    writer: T,
+    last_timestamp: Timestamp,
+    last_value: Value,
+    last_deltas: (i64, i64),
+    entries_written: u32,
 
-        let mut i = 0;
-        while (i < values.len() / 4 * 4) {
-            let mut length = 0u8;
-            let mut bytes_needed: u8;
-            for j in 0..4 {
-                bytes_needed = Self::bytes_needed_u64(values[i + j]);
-                length |= Self::length_encoding(bytes_needed) << (6 - 2 * j);
-            }
+    buffer: [u64; 4],
+    buffer_idx: usize,
 
-            result.push(length);
-            for j in 0..4 {
-                Self::encode_value(values[i + j], &mut result);
-            }
-            i += 4;
+    result: Vec<u8>,
+}
+
+impl<T: Write> CompressionEngine<T> {
+    pub fn new(writer: T, header: &Header) -> Self {
+        Self {
+            writer,
+            last_timestamp: header.min_timestamp,
+            last_value: header.first_value,
+            last_deltas: (0, 0),
+            entries_written: 0,
+
+            buffer: [0; 4],
+            buffer_idx: 0,
+            result: Vec::new(),
+        }
+    }
+
+    pub fn consume(&mut self, timestamp: Timestamp, value: Value) {
+        let curr_deltas = (
+            (timestamp.wrapping_sub(self.last_timestamp)) as i64,
+            (value.wrapping_sub(self.last_value)) as i64,
+        );
+
+        let double_delta_1 = curr_deltas.0 - self.last_deltas.0;
+        let double_delta_2 = curr_deltas.1 - self.last_deltas.1;
+        self.buffer[self.buffer_idx] = CompressionUtils::zig_zag_encode(double_delta_1);
+        self.buffer[self.buffer_idx + 1] = CompressionUtils::zig_zag_encode(double_delta_2);
+
+        self.buffer_idx += 2;
+        if self.buffer_idx >= self.buffer.len() {
+            self.flush();
         }
 
-        if values.len() % 4 > 0 {
-            let mut length = 0u8;
-            for i in (values.len() - (values.len() % 4))..values.len() {
-                let bytes_needed = Self::bytes_needed_u64(values[i]);
-                length |= Self::length_encoding(bytes_needed)
-                    << (6 - 2 * (i - (values.len() - (values.len() % 4))));
-            }
-            result.push(length);
+        self.entries_written += 1;
+        self.last_timestamp = timestamp;
+        self.last_value = value;
+        self.last_deltas = curr_deltas;
+    }
+
+    pub fn bytes_compressed(&self) -> usize {
+        self.result.len()
+    }
+
+    pub fn flush_all(&mut self) {
+        self.flush();
+        self.writer.write_all(&self.result).unwrap();
+    }
+
+    fn flush(&mut self) {
+        if self.buffer_idx == 0 {
+            return;
         }
 
-        for val in values[(values.len() - (values.len() % 4))..].iter() {
-            Self::encode_value(*val, &mut result);
+        let mut length = 0u8;
+        let mut bytes_needed: u8;
+
+        for j in 0..self.buffer_idx {
+            bytes_needed = Self::bytes_needed_u64(self.buffer[j]);
+            length |= Self::length_encoding(bytes_needed) << (6 - 2 * j);
+        }
+        self.result.push(length);
+
+        for i in 0..self.buffer_idx {
+            self.encode_value(self.buffer[i]);
         }
 
         self.buffer_idx = 0;
@@ -205,105 +235,6 @@ impl<T: Read> DecompressionEngine<T> {
             );
             buf_offset += EXPONENTS[indexes[i]];
             decoded_deltas[i] = CompressionUtils::zig_zag_decode(encoded);
-        }
-
-        // compute timestamp / value
-        self.last_deltas.0 += decoded_deltas[0];
-        self.current_timestamp = self
-            .current_timestamp
-            .wrapping_add_signed(self.last_deltas.0);
-
-        self.last_deltas.1 += decoded_deltas[1];
-        self.value = self.value.wrapping_add_signed(self.last_deltas.1);
-
-        // compute next timestamp / value
-        self.last_deltas.0 += decoded_deltas[2];
-        self.next_timestamp = self
-            .current_timestamp
-            .wrapping_add_signed(self.last_deltas.0);
-
-        self.last_deltas.1 += decoded_deltas[3];
-        self.next_value = self.value.wrapping_add_signed(self.last_deltas.1);
-
-        // update state
-        self.values_read += 1;
-        self.cur_length_byte = buffer[total_varint_lengths];
-
-        (self.current_timestamp, self.value)
-    }
-}
-
-pub struct DecompressionEngine<T: Read> {
-    reader: T,
-
-    values_read: u32,
-    cur_length_byte: u8,
-
-    current_timestamp: Timestamp,
-    value: Value,
-    last_deltas: (i64, i64),
-
-    next_timestamp: Timestamp,
-    next_value: Value,
-}
-
-impl<T: Read> DecompressionEngine<T> {
-    pub fn new(mut reader: T, header: &Header) -> Self {
-        let mut l_buf = [0u8; 1];
-        if header.count > 1 {
-            reader.read_exact(&mut l_buf).unwrap();
-        }
-        Self {
-            reader,
-
-            values_read: 1,
-            cur_length_byte: l_buf[0],
-
-            current_timestamp: header.min_timestamp,
-            value: header.first_value,
-            last_deltas: (0, 0),
-            next_timestamp: 0,
-            next_value: 0,
-        }
-    }
-
-    #[allow(clippy::should_implement_trait)]
-    pub fn next(&mut self) -> (Timestamp, Value) {
-        if self.values_read % 2 == 0 {
-            self.current_timestamp = self.next_timestamp;
-            self.value = self.next_value;
-            self.values_read += 1;
-            return (self.current_timestamp, self.value);
-        }
-
-        // compute integer lengths
-        let indexes: [usize; 4] = [
-            ((self.cur_length_byte >> 6) & 0b11) as usize,
-            ((self.cur_length_byte >> 4) & 0b11) as usize,
-            ((self.cur_length_byte >> 2) & 0b11) as usize,
-            ((self.cur_length_byte) & 0b11) as usize,
-        ];
-
-        let total_varint_lengths = EXPONENTS[indexes[0]]
-            + EXPONENTS[indexes[1]]
-            + EXPONENTS[indexes[2]]
-            + EXPONENTS[indexes[3]];
-
-        // read deltas + next length byte
-        let mut buffer = [0u8; 2 * (size_of::<Timestamp>() + size_of::<Value>()) + 1];
-        self.reader
-            .read_exact(&mut buffer[0..total_varint_lengths + 1])
-            .unwrap();
-
-        let mut decoded_deltas = [0i64; 4];
-        let mut buf_offset = 0;
-
-        for i in 0..4 {
-            let encoded = VAR_U64_READERS[indexes[i]](
-                &buffer[buf_offset..buf_offset + EXPONENTS[indexes[i]]],
-            );
-            buf_offset += EXPONENTS[indexes[i]];
-            decoded_deltas[i] = CompressionEngine::zig_zag_decode(encoded);
         }
 
         // compute timestamp / value
