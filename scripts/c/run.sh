7602e3e3338460be403fc18a610171e4ef7bdda6
Rajat Patwari <rajatpatwari@gmail.com>
2024-07-18 15:19:23 -0400

    Remove debug statements

--- scripts/c/run.sh
+++ scripts/c/run.sh
@@ -2,20 +2,6 @@
 
 set -euxo pipefail
 
-ls -al ./
-echo $'DONE\nDONE'
-ls -al ../
-echo $'DONE\nDONE'
-ls -al ../../
-echo $'DONE\nDONE'
-ls -al ../../target/
-echo $'DONE\nDONE'
-ls -al ../../target/release/
-echo $'DONE\nDONE'
-
-objdump -dC ../../target/release/libtachyon.so | grep "tachyon_"
-echo $'DONE\nDONE'
-
 gcc -v -O3 -I../../include -L../../target/release/ ./test.c -ltachyon -o test.out
 
 LD_LIBRARY_PATH=../../target/release/ ./test.out
