dd646b62b66968881e2d7418b907074db0bc31c9
Rajat Patwari <rajatpatwari@gmail.com>
2024-07-22 11:56:30 -0400

    Remove release compile flags

--- scripts/c/run.sh
+++ scripts/c/run.sh
@@ -2,8 +2,8 @@
 
 set -euxo pipefail
 
-gcc -v -O3 -I../../include -L../../target/release/ ./test.c -ltachyon -o test.out
+gcc -v -I../../include -L../../target/debug/ ./test.c -ltachyon -o test.out
 
-LD_LIBRARY_PATH=../../target/release/ ./test.out
+LD_LIBRARY_PATH=../../target/debug/ ./test.out
 
 rm -rf ./test.out ./test_db
