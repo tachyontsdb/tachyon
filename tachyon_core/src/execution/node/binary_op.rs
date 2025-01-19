be9e319c7f437f40c7b038e6a5b2fc9f96330bfc
Rick Liu <ricksliu.mail@gmail.com>
2025-01-19 04:17:06 -0500

    Implement comparison operators for vector to scalar

--- tachyon_core/src/execution/node/binary_op.rs
+++ tachyon_core/src/execution/node/binary_op.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 use crate::{Connection, ReturnType, Value, ValueType, Vector};
 
 use super::{ExecutorNode, ScalarToScalarNode, TNode, VectorToScalarNode, VectorToVectorNode};
@@ -9,9 +11,37 @@ pub enum BinaryOp {
     Multiply,
     Divide,
     Modulo,
+    Equal,
+    NotEqual,
+    Greater,
+    Less,
+    GreaterEqual,
+    LessEqual,
+}
+
+#[derive(Debug)]
+pub enum BinaryOpType {
+    Arithmetic,
+    Comparison,
 }
 
 impl BinaryOp {
+    pub fn op_type(&self) -> BinaryOpType {
+        match self {
+            BinaryOp::Add
+            | BinaryOp::Subtract
+            | BinaryOp::Multiply
+            | BinaryOp::Divide
+            | BinaryOp::Modulo => BinaryOpType::Arithmetic,
+            BinaryOp::Equal
+            | BinaryOp::NotEqual
+            | BinaryOp::Greater
+            | BinaryOp::Less
+            | BinaryOp::GreaterEqual
+            | BinaryOp::LessEqual => BinaryOpType::Comparison,
+        }
+    }
+
     pub fn apply(
         &self,
         lhs: Value,
@@ -25,6 +55,35 @@ impl BinaryOp {
             BinaryOp::Multiply => lhs.mul(lhs_value_type, &rhs, rhs_value_type),
             BinaryOp::Divide => lhs.div(lhs_value_type, &rhs, rhs_value_type),
             BinaryOp::Modulo => lhs.mdl(lhs_value_type, &rhs, rhs_value_type),
+            _ => panic!("apply not implemented for this binary operator!"),
+        }
+    }
+
+    pub fn compare(
+        &self,
+        lhs: Value,
+        lhs_value_type: ValueType,
+        rhs: Value,
+        rhs_value_type: ValueType,
+    ) -> bool {
+        match self {
+            BinaryOp::Equal => lhs.eq(lhs_value_type, &rhs, rhs_value_type),
+            BinaryOp::NotEqual => !lhs.eq(lhs_value_type, &rhs, rhs_value_type),
+            BinaryOp::Greater => {
+                lhs.partial_cmp(lhs_value_type, &rhs, rhs_value_type) == Some(Ordering::Greater)
+            }
+            BinaryOp::Less => {
+                lhs.partial_cmp(lhs_value_type, &rhs, rhs_value_type) == Some(Ordering::Less)
+            }
+            BinaryOp::GreaterEqual => {
+                let ordering = lhs.partial_cmp(lhs_value_type, &rhs, rhs_value_type);
+                ordering == Some(Ordering::Greater) || ordering == Some(Ordering::Equal)
+            }
+            BinaryOp::LessEqual => {
+                let ordering = lhs.partial_cmp(lhs_value_type, &rhs, rhs_value_type);
+                ordering == Some(Ordering::Less) || ordering == Some(Ordering::Equal)
+            }
+            _ => panic!("compare not implemented for this binary operator!"),
         }
     }
 }
