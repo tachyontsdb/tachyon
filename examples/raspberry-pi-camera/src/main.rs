b9b8fb63d6421499e09e832899a2027dbf0d0664
Srihari Vishnu <srihari.vishnu@gmail.com>
2025-03-15 18:37:17 -0400

    try

--- examples/raspberry-pi-camera/src/main.rs
+++ examples/raspberry-pi-camera/src/main.rs
@@ -52,8 +52,8 @@ fn main() -> Result<()> {
 
     // Try to open the default camera (index 0)
     let camera_index = CameraIndex::Index(0);
-    let requested_format = RequestedFormat::new::<RgbFormat>(RequestedFormatType::Exact(
-        CameraFormat::new(Resolution::new(width, height), FrameFormat::RAWRGB, fps),
+    let requested_format = RequestedFormat::new::<YuyvFormat>(RequestedFormatType::Exact(
+        CameraFormat::new(Resolution::new(width, height), FrameFormat::YUYV, fps),
     ));
     // let requested_format = RequestedFormat::new::<YuyvFormat>(requested_format_type);
 
