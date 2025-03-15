20821603fb9cb9624329ab12c57c5608c215bc51
Srihari Vishnu <srihari.vishnu@gmail.com>
2025-03-15 18:55:12 -0400

    save

--- examples/raspberry-pi-camera/test.py
+++ examples/raspberry-pi-camera/test.py
@@ -1,6 +1,15 @@
-from picamzero import Camera
+#!/usr/bin/python3
+import time
 
-cam = Camera()
+from picamera2 import Picamera2
+from picamera2.encoders import H264Encoder
 
-# Use a Picamera2 method
-cam.pc2.start_encoder()
+picam2 = Picamera2()
+video_config = picam2.create_video_configuration()
+picam2.configure(video_config)
+
+encoder = H264Encoder(10000000)
+
+picam2.start_recording(encoder, "test.h264")
+time.sleep(10)
+picam2.stop_recording()
