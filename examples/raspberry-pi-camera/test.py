8971e63bd3556ffc97cc5271b8c5ae720a58414c
Srihari Vishnu <srihari.vishnu@gmail.com>
2025-03-15 18:52:55 -0400

    try

--- examples/raspberry-pi-camera/test.py
+++ examples/raspberry-pi-camera/test.py
@@ -1,51 +1,6 @@
-import cv2
-from picamera import PiCamera
-from picamera.array import PiRGBArray
+from picamzero import Camera
 
-CAMERA_RESOLUTION = (640, 480)
+cam = Camera()
 
-
-def capture_video():
-    # Press 'q' to stop the program.
-    try:
-        # Setup
-        camera = PiCamera()
-        camera.resolution = CAMERA_RESOLUTION
-        output = PiRGBArray(camera)
-        # Update and draw
-        is_running = True
-        while is_running:
-            camera.capture(output, "bgr", use_video_port=True)
-            image = output.array
-            output.truncate(0)
-            # Display on screen
-            cv2.imshow("frame", image)
-            # Exit the loop on event
-            key = cv2.waitKey(10) & 0xFF
-            if key == ord("q"):
-                is_running = False
-    finally:
-        # Release resources
-        output.close()
-        camera.close()
-
-
-def capture_still():
-    try:
-        # Setup
-        camera = PiCamera()
-        output = PiRGBArray(camera)
-        # Capture
-        camera.capture(output, format="bgr", use_video_port=False)
-        frame = output.array
-        # Save the image to disk.
-        cv2.imwrite("frame.jpg", frame)
-    finally:
-        # Release resources
-        output.close()
-        camera.close()
-
-
-if __name__ == "__main__":
-    # capture_still()
-    capture_video()
+# Use a Picamera2 method
+cam.pc2.start_encoder()
