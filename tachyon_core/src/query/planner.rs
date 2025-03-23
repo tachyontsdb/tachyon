1d15dec4bf0e2ae46eefd542fac0fc7687f743de
Rick Liu <ricksliu.mail@gmail.com>
2025-03-23 13:49:13 -0400

    rebase changes

--- tachyon_core/src/query/planner.rs
+++ tachyon_core/src/query/planner.rs
@@ -31,21 +31,6 @@ impl<'a> QueryPlanner<'a> {
         expr: &AggregateExpr,
         conn: &mut Connection,
     ) -> Result<TNode, QueryErr> {
-        let start = if let Some(start) = self.start {
-            start
-        } else {
-            return Err(QueryErr::StartEndTimeErr {
-                start_or_end: "start".to_string(),
-            });
-        };
-        let end = if let Some(end) = self.end {
-            end
-        } else {
-            return Err(QueryErr::StartEndTimeErr {
-                start_or_end: "end".to_string(),
-            });
-        };
-
         match expr.op.id() {
             parser::token::T_SUM
             | parser::token::T_COUNT
@@ -62,8 +47,8 @@ impl<'a> QueryPlanner<'a> {
                 Ok(TNode::Aggregate(AggregateNode::new(
                     aggregate_type,
                     expr.subperiod,
-                    start,
-                    end,
+                    self.start,
+                    self.end,
                     Box::new(self.handle_expr(
                         &expr.expr,
                         conn,
@@ -80,8 +65,8 @@ impl<'a> QueryPlanner<'a> {
             parser::token::T_AVG => Ok(TNode::Aggregate(AggregateNode::new(
                 AggregateType::Average,
                 expr.subperiod,
-                start,
-                end,
+                self.start,
+                self.end,
                 Box::new(self.handle_expr(
                     &expr.expr,
                     conn,
